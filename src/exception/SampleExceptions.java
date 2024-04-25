b7323ee129c7046eb3249ba1bd73a3ca1a1b44c5
Jun Sasaki <iajunsasaki@gmail.com>
2024-04-26 08:36:51 +0900

    回答例

--- src/exception/SampleExceptions.java
+++ src/exception/SampleExceptions.java
@@ -8,23 +8,41 @@ public class SampleExceptions {
 		// 演習１：try/catch。演習２－４のコードをtry/catch文で囲んでみてください。
 		// 演習２：なぜ例外が起きるか原因を調べてください。そして8行目を変更することにより
 		//         エラーが発生しないようにしてください
-		String nullString = null;
-		System.out.println(cookString(nullString));
-		
-		// 演習３：なぜ例外が起きるか原因を調べてください。そして13行目を変更することにより
-		//         エラーが発生しないようにしてください
-		String oneCharString = "a";
-		System.out.println(cookString(oneCharString));
-		
-		// 演習４．１：以下のコード（「ワンモアマイル」まで）、そしてSimpleFileReaderクラス内で何がどのような順番で
-		//         処理されているかコメント文を書いてみましょう
-		// 演習４．２：テキスト１－４　リソース付try文を使ってSimpleFileReaderのreadTextFileメソッドを書き直してください
-		int number = 0;
-		System.out.println("割り算結果：" + (10 / number));
+		try {
+			//String nullString = null;
+			// null以外の２文字以上にするとエラー回避
+			// 中身がnullの変数でメソッド呼び出し、変数アクセスするとNullPointerException!
+			String nullString = "XXここから表示される";
+			System.out.println(cookString(nullString));
+			
+			// 演習３：なぜ例外が起きるか原因を調べてください。そして13行目を変更することにより
+			//         エラーが発生しないようにしてください
+			// null以外の２文字以上にするとエラー回避
+			//String oneCharString = "a";
+			String oneCharString = "aaエラー起きません";
+			System.out.println(cookString(oneCharString));
+			
+			// 演習４．１：以下のコード（「ワンモアマイル」まで）、そしてSimpleFileReaderクラス内で何がどのような順番で
+			//         処理されているかコメント文を書いてみましょう
+			// 演習４．２：テキスト１－４　リソース付try文を使ってSimpleFileReaderのreadTextFileメソッドを書き直してください
+			// int型の割り算はゼロで割ると例外発生する
+			// int number = 0;
+			int number = -1;
+			System.out.println("割り算結果：" + (10 / number));
+		}
+		catch (Exception exception) {
+			// こうすることでコンソールにエラー詳細が出力される
+			exception.printStackTrace();
+		}
 
 		// ファイル処理
 		SimpleFileReader fileReader = new SimpleFileReader();
+		
+		// 引数にファイルの場所を指定する。
+		// このメソッド内でファイルを開き、読み込みを行う。例外が発生するとすればここが怪しい
 		fileReader.readTextFile("users.txt");
+		
+		// すでにこの時点で読み込みは終了。読み込んだデータをgetterで取得する
 		ArrayList<String> lines = fileReader.getLines();
 		
 		int lineCount = 1;
@@ -36,6 +54,7 @@ public class SampleExceptions {
 		
 		// ワンモアマイル：テキストの内容やネットでのリサーチ情報を活用して、
 		// SimpleFileReaderクラスにあるwriteTextFileを書いてみてください。
+		fileReader.writeTextFile("usersver2.txt");
 	}
 
 	/**
